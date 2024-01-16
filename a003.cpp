fbcf654f0afa5b20926fa07f74b3d665d3edcc7c
nan786521 <o.cless@gmail.com>
2024-01-16 09:07:28 +0800

    Create a003.cpp

---
+++ a003.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int M,D,S;
+    cin >> M >> D;
+    S=(M*2+D)%3;
+    if (S==0) {
+        cout << "´¶³q";
+    }
+    else if (S==1)
+    {
+        cout << "¦N";
+    }
+    else
+    {
+        cout << "¤j¦N";
+    }
+}
